dcbe20a30d1ebd7384a2c79f1754eae547360cde
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2023-10-09 16:54:06 +0200

    New manual translations, translated manual, and more (#1)
    * Add .gitignore
    
    * Add new hand translations
    
    * Apply new hand translations to resources
    
    * Add F2 hotkey screen translation
    
    * Add title screen translation + upscale
    
    * Update gitignore
    
    * Typo fix
    
    * Improve some existing UI translations
    
    * UI TL improvements
    
    * Fix Setings -> Settings
    
    * Minor ADV fixes
    
    * Add better quality PointCardBG
    
    * Add translated manual
    
    * Add companion plugin

---
+++ TweaksPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using IO_Tweaks;
+
+[assembly: AssemblyTitle(TweaksPlugin.DisplayName)]
+[assembly: AssemblyDescription(TweaksPlugin.DisplayName)]
+[assembly: AssemblyCompany("ManlyMarco")]
+[assembly: AssemblyProduct(TweaksPlugin.DisplayName)]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+
+
+[assembly: ComVisible(false)]
+[assembly: Guid("0dadf401-fd7f-4140-a8e5-3987014e4f41")]
+
+
+[assembly: AssemblyVersion(TweaksPlugin.Version)]
